fe90f2bbde450f83b75d4045ee1d2d9929bd5fa4
drakemj <drakemj@uci.edu>
2022-11-07 17:26:32 -0800

    fix ugly loadassets function

--- src/Graphics.py
+++ src/Graphics.py
@@ -7,37 +7,23 @@ def calculateSquare(pos, board, size):
     return (7 - int(pos[0]/size), 7 - int(8 - pos[1]/size))
 
 def loadAssets(size):
-    whitePawn = pygame.image.load("src/assets/Chess_plt60.png")
-    whitePawn = pygame.transform.scale(whitePawn, (size, size))
-    blackPawn = pygame.image.load("src/assets/Chess_pdt60.png")
-    blackPawn = pygame.transform.scale(blackPawn, (size, size))
-    whiteRook = pygame.image.load("src/assets/Chess_rlt60.png")
-    whiteRook = pygame.transform.scale(whiteRook, (size, size))
-    blackRook = pygame.image.load("src/assets/Chess_rdt60.png")
-    blackRook = pygame.transform.scale(blackRook, (size, size))
-    whiteKnight = pygame.image.load("src/assets/Chess_nlt60.png")
-    whiteKnight = pygame.transform.scale(whiteKnight, (size, size))
-    blackKnight = pygame.image.load("src/assets/Chess_ndt60.png")
-    blackKnight = pygame.transform.scale(blackKnight, (size, size))
-    whiteBishop = pygame.image.load("src/assets/Chess_blt60.png")
-    whiteBishop = pygame.transform.scale(whiteBishop, (size, size))
-    blackBishop = pygame.image.load("src/assets/Chess_bdt60.png")
-    blackBishop = pygame.transform.scale(blackBishop, (size, size))
-    whiteQueen = pygame.image.load("src/assets/Chess_qlt60.png")
-    whiteQueen = pygame.transform.scale(whiteQueen, (size, size))
-    blackQueen = pygame.image.load("src/assets/Chess_qdt60.png")
-    blackQueen = pygame.transform.scale(blackQueen, (size, size))
-    whiteKing = pygame.image.load("src/assets/Chess_klt60.png")
-    whiteKing = pygame.transform.scale(whiteKing, (size, size))
-    blackKing = pygame.image.load("src/assets/Chess_kdt60.png")
-    blackKing = pygame.transform.scale(blackKing, (size, size))
+    o = [None for i in range(17)]
+    team = ['l', 'd']
+    pieces = [None, 'p', 'r', 'n', 'b', 'q', 'k']
+
+    for i in range(1, 7):
+        for j in range(2):
+            o[i + 10*j] = pygame.transform.scale(pygame.image.load("src/assets/Chess_" + pieces[i] + team[j] + "t60.png"), (size, size))
+
     whiteSquare = pygame.image.load("src/assets/whiteSq.png")
     whiteSquare = pygame.transform.scale(whiteSquare, (size, size))
     blackSquare = pygame.image.load("src/assets/blackSq.png")
     blackSquare = pygame.transform.scale(blackSquare, (size, size))
 
-    return [None, whitePawn, whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteSquare, blackSquare, None, None, blackPawn, blackRook, 
-    blackKnight, blackBishop, blackQueen, blackKing]
+    o[7] = whiteSquare
+    o[8] = blackSquare
+
+    return o
 
 def printBoard(screen, assets, board, size):
     screen.fill((255, 255, 255), (0, 0, size*8, size*8))
