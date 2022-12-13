3b45485ae826ae4d512c01466fb299c6e817bede
drakemj <drakemj@uci.edu>
2022-12-12 17:36:15 -0800

    add check sound

--- src/Sound.py
+++ src/Sound.py
@@ -6,10 +6,11 @@ class SoundMixer:
     def __init__(self):
         self.sounds.append(pygame.mixer.Sound("src/assets/sounds/capture.mp3"))
         self.sounds.append(pygame.mixer.Sound("src/assets/sounds/move-self.mp3"))
+        self.sounds.append(pygame.mixer.Sound("src/assets/sounds/check.mp3"))
 
     def playMove(self, capture, check):
         if check:
-            self.sounds[0].play(1)
+            self.sounds[2].play()
         elif capture:
             self.sounds[0].play()
         else:
