efebe656072ca406b034c04a77a273bbeb005aed
drakemj <drakemj@uci.edu>
2022-12-14 19:14:46 -0800

    add promotion

--- src/Parse.py
+++ src/Parse.py
@@ -1,15 +1,6 @@
 import chess
 import chess.pgn
 
-game1 = chess.Board()
-print(game1)
-nmove = chess.Move.from_uci("e2e4")
-game1.push(nmove)
-print(game1)
-game1.pop()
-print("hi")
-print(game1)
-
 class GameStack:
     game = chess.Board()
 
@@ -17,8 +8,9 @@ class GameStack:
         game = chess.Board()
 
     def pushMove(self, start, end, promote):
+        select = ['q', 'r', 'n', 'b']
         s = "{0}{1}{2}{3}".format(chr(ord('a') + start[0]), start[1] + 1, chr(ord('a') + end[0]), end[1] + 1)
-        print(s)
+        if(promote != None): s += select[promote]
         move = chess.Move.from_uci(s)
         self.game.push(move)
         print(self.game)
